6564a597605d166403f02f649171548752bf26f2
Vishnu Vardhan Reddy Maddula <82017895+mvvr@users.noreply.github.com>
2024-09-14 07:08:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 import streamlit as st
+import cv2
+import tempfile
 from utils import image_processor, description_generator
 
 def main():
     st.title("Intelligent Scene Description for the Visually Impaired")
     
-    video_input = st.file_uploader("Upload a video", type=["mp4", "avi"])
+    video_input = st.file_uploader("Upload a video", type=["mp4", "avi", "mov"])
     
     if video_input is not None:
-        # Process the video and get scene description
-        frames = image_processor.extract_frames(video_input)
+        # Save the uploaded file temporarily
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(video_input.read())
+            temp_video_path = temp_file.name
+
+        # Now, pass the temp_video_path to OpenCV
+        frames = image_processor.extract_frames(temp_video_path)
+        
         descriptions = description_generator.generate_descriptions(frames)
         
         for description in descriptions:
